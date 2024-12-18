8018978398a47d14717ad31ff1b50c56f68e6cdb
luludocteur <luludocteurcrypt@gmail.com>
2024-12-18 23:34:59 +0100

    Dev: model_maker (bcp de chose a changer, pas opti)

--- src/fonctions/fonctions_modelisations.py
+++ src/fonctions/fonctions_modelisations.py
@@ -10,48 +10,6 @@ from fonctions_traitement import data_v1
 load_dotenv()
 wd = os.getenv("working_directory")
 
-def mod_random_forest(df):
-    X = df.drop(columns=["purchase_price"])
-    y = df['purchase_price']
-
-    # Création des dummies pour les variables catégorielles
-    X_non_numeric = X.select_dtypes(exclude=['number'])
-    X_numeric = X.select_dtypes(include=['number'])
-    X_dummies = pd.get_dummies(X_non_numeric)
-    X = pd.concat([X_numeric, X_dummies], axis=1)
-
-    # Division des données en ensembles d'entraînement et de test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2, random_state=42)
-
-    # Grille de paramètres pour GridSearchCV
-    param_grid = {
-    'n_estimators': [50, 100, 200],
-    'max_depth': [None, 10, 20],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
-    'max_features': ['auto', 'sqrt'],
-    'bootstrap': [True, False]
-    }
-
-    random_forest = RandomForestRegressor(random_state=42)
-
-    grid_search = GridSearchCV(random_forest,param_grid,cv=5,scoring='r2',n_jobs=1
-                               ) # verbose => niveau d'affichage des messages pendant l'execution
-
-    grid_search.fit(X_train, y_train)
-
-    best_model = grid_search.best_estimator_
-
-    y_pred = best_model.predict(X_test)
-
-    # score 
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-
-    print(f"Mean Squared Error : {mse}")
-    print(f"R² Score :{r2}")
-
-
 def Create_Train_Test(df):
     """
     Créer les dataframes de test et d'entraînement 
@@ -70,29 +28,20 @@ def Grid_Search_RFR(X_train, y_train):
     Compute le RandomForestRegressor en appliquant un GridSearchCV
     """
     param_grid = {
-    'n_estimators': [50, 100, 200],
-    'max_depth': [10, 20],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
-    'max_features': ['log2', 'sqrt'],
-    'bootstrap': [True, False]
+    'criterion' : ["squared_error", "friedman_mse"],
+    'n_estimators': [200],
+    'max_depth': [20, 30],
+    'min_samples_split': [3,4,5],
+    'max_features': ['sqrt'],
+    'bootstrap': [False]
     }
 
     random_forest = RandomForestRegressor(random_state=42)
-    grid_search = GridSearchCV(random_forest,param_grid,cv=5,scoring='r2',n_jobs=7)
+    grid_search = GridSearchCV(random_forest,param_grid,cv=3,scoring='r2',n_jobs=4)
     grid_search.fit(X_train, y_train)
     best_model = grid_search.best_estimator_
 
-    results = pd.DataFrame(grid_search.cv_results_)
-
-    # Trier les résultats par la meilleure performance (MSE le plus élevé)
-    best_results = results.sort_values(by='mean_test_score', ascending=False)
-
-    # Afficher les 10 meilleures combinaisons d'hyperparamètres
-    top_10_params = best_results[['params', 'mean_test_score']].head(10)
-    print(top_10_params)
-
-    return best_model
+    return random_forest, best_model
 
 def Grid_Search_LR(X_train, y_train):
     """
@@ -103,29 +52,27 @@ def Grid_Search_LR(X_train, y_train):
     }
 
     linear_regressor = LinearRegression()
-    grid_search = GridSearchCV(linear_regressor,param_grid,cv=5,scoring='r2',n_jobs=7)
+    grid_search = GridSearchCV(linear_regressor,param_grid,cv=5,scoring='r2',n_jobs=5)
     grid_search.fit(X_train, y_train)
     best_model = grid_search.best_estimator_
 
-    return best_model
+    return linear_regressor, best_model
 
-def random_forest_regressor(X_train, y_train, params=None):
+def random_forest_regressor(params=None):
     """
     Compute Random Forest Regressor et retourne le modèle entraîné
     """
     if params is None:
         params = {}
-    model = RandomForestRegressor(random_state=42, **params).fit(X_train, y_train)
+    model = RandomForestRegressor(random_state=42, **params)
     
     return model
 
-def linear_regressor(X_train, y_train, params=None):
+def linear_regressor():
     """
     Compute Linear Regressor et retourne le modèle entrainé 
     """
-    if params is None:
-        params = {}
-    model = LinearRegression(**params).fit(X_train, y_train)
+    model = LinearRegression(fit_intercept=True)
 
     return model
 
@@ -152,12 +99,19 @@ def Score(model, X_test, y_test):
 def main():
     """
     """
-    df = pd.read_csv(f'{wd}/data/data_year/DKHousing_1999.csv')
+    df = pd.read_parquet(f'{wd}/data/concat_year/DKHousing_1992_2021.parquet')
     df_v1 = data_v1(df)
     X_train, X_test, y_train, y_test = Create_Train_Test(df_v1)
-    best_RFR = Grid_Search_RFR(X_train, y_train)
-    dico_metrics = Score(best_RFR, X_test, y_test)
+    params = {'bootstrap': False,
+              'criterion': 'squared_error',
+              'max_depth': 30,
+              'max_features': 'sqrt',
+              'min_samples_split': 4,
+              'n_estimators': 200}
+    RFR = random_forest_regressor(params)
+    RFR_fit = RFR.fit(X_train, y_train)
+    dico_metrics = Score(RFR, X_test, y_test)
+    print(RFR.get_params())
     print(dico_metrics)
 
 
-main()
--- src/model_maker.py
+++ src/model_maker.py
@@ -1,10 +1,95 @@
 import os
-from dotenv import load_dotenv
+import sys
 import pandas as pd
-import numpy as np
+import json
+from dotenv import load_dotenv
 
-from fonctions.fonctions_traitement import data_v1
-from fonctions.fonctions_modelisations import Create_Train_Test, Grid_Search_RFR, Grid_Search_LR, Score
 load_dotenv()
 wd = os.getenv("working_directory")
+sys.path.append(f'{wd}/src/fonctions')
+from fonctions.fonctions_traitement import data_v1
+from fonctions.fonctions_modelisations import Create_Train_Test, random_forest_regressor, linear_regressor, Score
+
+
+
+def RFR_fct(X_train, X_test, y_train, y_test):
+    """
+    Applique la modelisation pour RandomForestRegressor
+    Retourne les paramètres et les métriques de qualités
+    """
+    params = {'bootstrap': False,
+              'criterion': 'squared_error',
+              'max_depth': 30,
+              'max_features': 'sqrt',
+              'min_samples_split': 4,
+              'n_estimators': 200}
+    
+    RFR = random_forest_regressor(params)
+    RFR_fit = RFR.fit(X_train, y_train)
+    params_RFR = RFR.get_params()
+    metrics_RFR = Score(RFR_fit, X_test, y_test)
+
+    dico_RFR = {'model': RFR, 'params':params_RFR, 'metrics': metrics_RFR}
+
+    return dico_RFR
+
+def LR_fct(X_train, X_test, y_train, y_test):
+    """
+    Applique la modelisation pour LinearRegression
+    Retourne les paramètres et les métriques de qualités
+    """
+    LR = linear_regressor()
+    LR_fit = LR.fit(X_train, y_train)
+    params_LR = LR.get_params()
+    metrics_LR = Score(LR_fit, X_test, y_test)
+
+    dico_LR = {'model': LR, 'params':params_LR, 'metrics': metrics_LR}
+
+    return dico_LR
+
+def training_models(dataset_path):
+    """
+    Applique les transformations de la fonction data_v1
+    df est split en données d'entrainement et de test (80/20)
+    Train les 2 modeles LinearRegression et RandomForestRegressor sur les données d'entrainement
+    Test sur les jeux de test
+    Retourne les métriques de qualités et les valeurs des hyperparamètres utilisés
+    """
+    dataset_name = dataset_path.split('/')[-1].split('.')[0]
+    print(dataset_name)
+    df = pd.read_parquet(dataset_path)
+    data_V1 = data_v1(df)
+    X_train, X_test, y_train, y_test = Create_Train_Test(data_V1)
+    
+    #RandomForestRegressor
+    dico_RFR = RFR_fct(X_train, X_test, y_train, y_test)
+    #LinearRegressor
+    dico_LR = LR_fct(X_train, X_test, y_train, y_test)
+
+    dico_dataset = {f'{dataset_name}_RFR':dico_RFR,
+                    f'{dataset_name}_LR':dico_LR}
+    
+    return dico_dataset
+
+def loop_training():
+    """
+    Applique la fonction training models sur plusieurs années
+    """
+    dico_master = {}
+    for year in list(range(2010, 2022+1)):
+        dataset_name = f'{wd}/data/concat_year/DKHousing_1992_{year}.parquet'
+        dico = training_models(dataset_name)
+        dico_master.update(dico)
+        with open(f'{wd}/data/data_models/tracking_model_{year}.json', 'w', encoding='utf-8') as fichier:
+            json.dump(dico, fichier, indent=4, ensure_ascii=False)
+        
+
+def main():
+    loop_training()
+    #print(training_models(f'{wd}/data/concat_year/DKHousing_1992_2021.parquet'))
+
+main()
+
+
+
 
